69e2dbe74879db7d0d69fb1d26925e8e892d2992
patatan168 <mashiko.3290@gmail.com>
2024-12-02 18:03:12 +0900

    🔧 storybookの名前解決(publicフォルダ)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
+  staticDirs: ['../public'],
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
