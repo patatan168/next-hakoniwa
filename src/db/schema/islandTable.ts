c9ec728c777bc9f2388b5617e933f777d3a0a34b
patatan168 <mashiko.3290@gmail.com>
2024-12-07 21:18:22 +0900

    ✨ JSONカラムをオブジェクトに変換する機能を追加

--- src/db/schema/islandTable.ts
+++ src/db/schema/islandTable.ts
@@ -79,6 +79,24 @@ export type islandSchemaType = {
   public_island_info: islandInfoData;
 };
 
+/**
+ * JSON ColumをObjectに変換
+ * @param island islandテーブルのデーター
+ */
+export const parseJsonIslandData = (island: unknown) => {
+  if (typeof island === 'object' && island !== null) {
+    if ('prize' in island && typeof island.prize === 'string') {
+      island.prize = JSON.parse(island.prize);
+    }
+    if ('island_info' in island && typeof island.island_info === 'string') {
+      island.island_info = JSON.parse(island.island_info);
+    }
+    if ('public_island_info' in island && typeof island.public_island_info === 'string') {
+      island.public_island_info = JSON.parse(island.public_island_info);
+    }
+  }
+};
+
 export type islandInfoData = Array<islandInfo>;
 
 export type islandInfo = {
