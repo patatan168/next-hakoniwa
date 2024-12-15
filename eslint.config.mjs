8f2abee6079fb4a52423e63e03deee0b24f1e3dc
patatan168 <mashiko.3290@gmail.com>
2024-12-15 16:30:38 +0900

    💥 ES-lint9マイグレーション/cookies-next更新

---
+++ eslint.config.mjs
@@ -0,0 +1,99 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import jsESLint from '@eslint/js';
+import typeScriptESLint from '@typescript-eslint/eslint-plugin';
+import typeScriptESLintParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tailwindcss from 'eslint-plugin-tailwindcss';
+import globals from 'globals';
+
+const compat = new FlatCompat();
+
+export default [
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      '.eslintrc.cjs',
+      'next.config.mjs',
+      'vitest.config.mts',
+      'tailwind.config.js',
+      'postcss.config.js',
+    ],
+  },
+  jsESLint.configs.recommended,
+  eslintConfigPrettier,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  ...tailwindcss.configs['flat/recommended'],
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:jsx-a11y/strict',
+    'plugin:storybook/recommended',
+    'prettier'
+  ),
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': typeScriptESLint,
+      typeScriptESLintParser,
+      jsxA11y,
+      prettier,
+      react,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: 2023,
+      parserOptions: {
+        project: ['tsconfig.json'],
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      parser: typeScriptESLintParser,
+      sourceType: 'module',
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        ...globals.browser,
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+    rules: {
+      // ===にしないとエラー
+      eqeqeq: 'error',
+      // サイクロマティック複雑度が10を超えるとエラー
+      complexity: ['error', 10],
+      // Any型は警告のとどめて許容する
+      '@typescript-eslint/no-explicit-any': 'warn',
+      // Childrenの一行タグを許可<Example />
+      'react/no-children-prop': 'error',
+      // useEffectの依存関係のWariningを無効
+      'react-hooks/exhaustive-deps': 'off',
+      // JSX に直接コールバック関数の記述を警告
+      // NOTE: render ごとに毎回新たな関数実体が生成されre-renderを引き起こすため
+      'react/jsx-no-bind': [
+        'error',
+        {
+          // アロー関数のコールバックは許可
+          allowArrowFunctions: true,
+        },
+      ],
+      // Componentの複数exportを警告
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // TailwindのカスタムCSSを許可
+      'tailwindcss/no-custom-classname': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
