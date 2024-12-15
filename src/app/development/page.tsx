88c9d888fd4df4aa3eba3efb50eca949b2dc2aab
patatan168 <mashiko.3290@gmail.com>
2024-12-16 08:11:31 +0900

    ♻️ Fetch処理の最適化

--- src/app/development/page.tsx
+++ src/app/development/page.tsx
@@ -1,25 +1,13 @@
 'use client';
-import { islandSchemaType } from '@/db/schema/islandTable';
 import HakoniwaMap from '@/global/component/HakoniwaMap';
 import { useFetch } from '@/global/function/fetch';
-import { useEffect, useState } from 'react';
 
 export default function IslandList() {
-  const [island, setIsland] = useState<islandSchemaType>();
   const { data } = useFetch('/api/auth/development', { method: 'GET' });
-
-  useEffect(() => {
-    if (data !== undefined) {
-      console.warn(data);
-      setIsland(data);
-    }
-  }, [data]);
 
   return (
     <>
-      {island !== undefined && (
-        <HakoniwaMap islandName={island.island_name} data={island.island_info} />
-      )}
+      {data !== undefined && <HakoniwaMap islandName={data.island_name} data={data.island_info} />}
     </>
   );
 }
