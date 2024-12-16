2dc172c88f26c8e10ef83fcdc067200b9cc69260
patatan168 <mashiko.3290@gmail.com>
2024-12-16 09:58:04 +0900

    🐛 HTMLのLanguageに誤りがあったので修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="jp">
+    <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   );
