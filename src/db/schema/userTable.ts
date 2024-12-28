73d76d22c728050329fbffb4a940ed21f8909c24
patatan168 <mashiko.3290@gmail.com>
2024-12-28 12:15:54 +0900

    ✨ userTableにinhabitedカラムを追加し、有人島かどうか管理

--- src/db/schema/userTable.ts
+++ src/db/schema/userTable.ts
@@ -2,6 +2,7 @@ import { DbSchema } from '@/global/function/db';
 
 export const userSchema: DbSchema = [
   { name: 'uuid', type: 'TEXT', primary: true, unique: true },
+  { name: 'inhabited', type: 'INTEGER', defVal: '1' },
   { name: 'id', type: 'TEXT', unique: true },
   { name: 'password', type: 'TEXT' },
   { name: 'island_name', type: 'TEXT', unique: true },
