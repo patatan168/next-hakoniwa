41583f461f96b12c1c593f5094b86f2df74490a8
patatan168 <mashiko.3290@gmail.com>
2025-07-14 21:08:29 +0900

    🎨 ApiErrorクラスにステータスコードとステータステキストを必須化

--- src/global/function/fetch.ts
+++ src/global/function/fetch.ts
@@ -6,7 +6,7 @@ class ApiError extends Error {
   status?: number;
   statusText?: string;
 
-  constructor(message: string, status?: number, statusText?: string) {
+  constructor(message: string, status: number, statusText: string) {
     super(message);
     this.name = 'ApiError';
     this.status = status;
