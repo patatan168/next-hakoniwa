30e765595de59c734482a995824f86ecbbbb8e26
patatan168 <mashiko.3290@gmail.com>
2025-07-15 07:09:33 +0900

    🐛 ApiErrorのstatusの必須化漏れ

--- src/global/function/fetch.ts
+++ src/global/function/fetch.ts
@@ -3,8 +3,8 @@ import useSWR from 'swr';
 import useSWRMutation, { SWRMutationResponse } from 'swr/mutation';
 
 class ApiError extends Error {
-  status?: number;
-  statusText?: string;
+  status: number;
+  statusText: string;
 
   constructor(message: string, status: number, statusText: string) {
     super(message);
