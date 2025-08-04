8a27cc33a7acf10d0ee416e68e782ae185aaa1b3
patatan168 <mashiko.3290@gmail.com>
2025-08-04 22:18:24 +0900

    ♻️ ミドルウェアにレートリミット機能を追加し、セッションチェックを統合

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,50 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { sessionStore } from './global/store/api/auth/session';
 
+const WINDOW_MS = 1 * 1000;
+const MAX_REQUESTS = 10;
+const ipMap = new Map<string, { count: number; timestamp: number }>();
+
+const rateLimitPaths = ['/api'];
+const sessionPaths = ['/development'];
+
 export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  if (rateLimitPaths.some((prefix) => pathname.startsWith(prefix))) {
+    return await rateLimit(request);
+  }
+  if (sessionPaths.some((prefix) => pathname.startsWith(prefix))) {
+    return await sessionCheck(request);
+  }
+  return NextResponse.next();
+}
+
+async function rateLimit(request: NextRequest) {
+  const ip = request.headers.get('x-forwarded-for') ?? 'unknown';
+
+  const now = Date.now();
+  const entry = ipMap.get(ip);
+  const isResetMap = !entry || now - entry.timestamp > WINDOW_MS;
+  let isLimit = true;
+
+  if (isResetMap) {
+    ipMap.set(ip, { count: 1, timestamp: now });
+  } else {
+    if (entry.count >= MAX_REQUESTS) {
+      isLimit = false;
+    } else {
+      entry.count += 1;
+    }
+  }
+
+  if (!isLimit) {
+    return new NextResponse('Too many requests', { status: 429 });
+  }
+
+  return NextResponse.next();
+}
+
+async function sessionCheck(request: NextRequest) {
   const cookie = request.headers.get('cookie') ?? '';
   await sessionStore
     .getState()
@@ -14,7 +57,3 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/unauthorized', request.url));
   }
 }
-
-export const config = {
-  matcher: ['/development/:path*'], // 認証が必要なルートを指定
-};
