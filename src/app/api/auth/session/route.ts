826c55f8c0cc6a90342b357f9e2c2d8b6b55ab5a
patatan168 <mashiko.3290@gmail.com>
2025-08-10 23:04:13 +0900

    🔒️  IPアドレスの抽出を追加し、逆引き不能なアドレスを弾くように強化

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,5 +1,6 @@
 import { validAuthCookie } from '@/global/function/auth';
 import { dbConn } from '@/global/function/db';
+import { extractClientIp } from '@/global/function/ip';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function OPTIONS() {
@@ -7,6 +8,10 @@ export async function OPTIONS() {
 }
 
 export async function GET(request: NextRequest) {
+  // 不正の疑いがあるIPアドレスはセッションを切る
+  const ip = extractClientIp(request);
+  if (!ip) return NextResponse.json({ error: 'IPアドレスが逆引きできません。' }, { status: 400 });
+  // セッションのバリデーション
   using db = dbConn('./src/db/data/main.db');
   const response = new NextResponse();
   const uuid = await validAuthCookie(db.client, response, request);
