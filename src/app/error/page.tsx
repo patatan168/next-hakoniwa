61a9693364e102635efd71cd753d094d6eaafef5
patatan168 <mashiko.3290@gmail.com>
2025-08-11 10:00:31 +0900

    ♻️ エラーページのコンポーネント名を修正し、整合性を向上

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function UnauthorizedPage() {
+export default function ErrorPage() {
   return (
     <div className="p-6 text-center text-red-600">
       <h1 className="text-xl font-bold">不明なエラーです。</h1>
