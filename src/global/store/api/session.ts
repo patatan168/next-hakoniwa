ad32a44bc07926504d7c5d3cde371439724d8abe
patatan168 <mashiko.3290@gmail.com>
2026-01-20 21:51:14 +0900

    feat: セッションAPIのストアを削除し、コードを整理

--- src/global/store/api/session.ts
+++
@@ -1,5 +0,0 @@
-import { FetchStore } from '@/global/function/fetch/fetch';
-
-const store = new FetchStore<{ uuid: string }>('/api/session', { waitTime: 300 });
-
-export const sessionStore = store.store;
