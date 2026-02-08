53a4a54b07b61832ef9187ac3b1d2577c1b4c038
patatan168 <mashiko.3290@gmail.com>
2026-02-08 21:18:56 +0900

    feat: uuidv25を独自実装

--- src/global/function/encrypt.ts
+++ src/global/function/encrypt.ts
@@ -1,7 +1,27 @@
 import crypto, { generateKeyPairSync } from 'crypto';
-import { Uuid25 } from 'uuid25';
 import { uuidv7obj } from 'uuidv7';
 import { secureRandom } from './utility';
+
+/**
+ * バイナリからUUID25を生成
+ * @param bin UUIDv7のバイナリ
+ * @returns UUID25
+ */
+function encodeUuid25FromBinary(bin: Uint8Array): string {
+  // バイナリ16バイトを1つの巨大な数 (BigInt) に変換
+  let num = 0n;
+  for (const byte of bin) {
+    num = (num << 8n) + BigInt(byte);
+  }
+  // Base62エンコード
+  let res = '';
+  while (num > 0n) {
+    const charset = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    res = charset[Number(num % 62n)] + res;
+    num = num / 62n;
+  }
+  return res.padStart(25, '0');
+}
 
 /**
  * UUIDを作成
@@ -9,8 +29,8 @@ import { secureRandom } from './utility';
  * @returns
  */
 export const createUuid25 = () => {
-  const uuid25 = Uuid25.fromBytes(uuidv7obj().bytes);
-  return uuid25.value;
+  const uuid25 = encodeUuid25FromBinary(uuidv7obj().bytes);
+  return uuid25;
 };
 
 /**
