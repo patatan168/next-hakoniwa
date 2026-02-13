877fd02862cc4ffcb66cf0ed938b5d2b7bed458d
patatan168 <mashiko.3290@gmail.com>
2026-02-13 12:52:26 +0900

    feat: フッターのデザイン調整

--- src/global/component/Footer.tsx
+++ src/global/component/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="text-gray-700">
       <div className="mx-1 grid grid-cols-4 grid-rows-none items-center gap-0 text-xs">
         {/* 左カラム */}
         <div className="col-span-2">Original Developer 徳岡宏樹</div>
@@ -13,7 +13,9 @@ export default function Footer() {
 
         {/* 右カラム */}
         <div className="col-span-1 flex justify-end">
-          <Link href="/license">Other License</Link>
+          <Link href="/license" className="underline">
+            Other License
+          </Link>
         </div>
       </div>
     </footer>
