941d935bf6abff7dda2966407b241306a108c455
patatan168 <mashiko.3290@gmail.com>
2026-02-14 18:03:14 +0900

    style: Refine title and version spacing, update global font, add text shadow to title, and remove dark mode specific CSS variables.

--- src/app/component/Title.tsx
+++ src/app/component/Title.tsx
@@ -4,8 +4,8 @@ import META from '@/global/define/metadata';
 export default function Title() {
   return (
     <>
-      <h1 className="title text-5xl">{META.TITLE}</h1>
-      <p className="title ml-4">{`Version: ${META.VERSION}`}</p>
+      <h1 className="title ml-1 text-5xl">{META.TITLE}</h1>
+      <p className="title ml-5">{`Version: ${META.VERSION}`}</p>
     </>
   );
 }
