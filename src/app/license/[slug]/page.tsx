a3372b1fe495c4d8f7737379bc54ec05bcdb06b7
patatan168 <mashiko.3290@gmail.com>
2026-07-10 19:42:16 +0900

    Fix:ライセンスページのデグレを修正

--- src/app/license/[slug]/page.tsx
+++ src/app/license/[slug]/page.tsx
@@ -7,8 +7,8 @@ import { MDXRemote } from 'next-mdx-remote-client/rsc';
 import { notFound } from 'next/navigation';
 import path from 'path';
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+export default async function Page({ params }: PageProps<'/license/[slug]'>) {
+  const { slug } = await params;
   const filePath = path.join(process.cwd(), 'src', 'app', 'license', 'md', `${slug}.mdx`);
   if (!fs.existsSync(filePath)) {
     notFound();
