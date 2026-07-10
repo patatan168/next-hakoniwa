7b9273ead6831d19c6c35ec63973887d7424486d
patatan168 <mashiko.3290@gmail.com>
2026-07-10 19:32:15 +0900

    Fix: 型のエラーを修正

--- src/app/license/[slug]/page.tsx
+++ src/app/license/[slug]/page.tsx
@@ -7,8 +7,8 @@ import { MDXRemote } from 'next-mdx-remote-client/rsc';
 import { notFound } from 'next/navigation';
 import path from 'path';
 
-export default async function Page({ params }: PageProps<'/license/[slug]'>) {
-  const { slug } = await params;
+export default async function Page({ params }: { params: { slug: string } }) {
+  const { slug } = params;
   const filePath = path.join(process.cwd(), 'src', 'app', 'license', 'md', `${slug}.mdx`);
   if (!fs.existsSync(filePath)) {
     notFound();
